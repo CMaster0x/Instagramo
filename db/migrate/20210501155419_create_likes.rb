7ec2a5c0b0eb83f67701a3080243c4ec9fdcae87
CMaster0x <cesar.perezvuelvas95@gmail.com>
2021-05-08 13:36:23 -0500

    Fixes migration version

--- db/migrate/20210501155419_create_likes.rb
+++ db/migrate/20210501155419_create_likes.rb
@@ -1,4 +1,4 @@
-class CreateLikes < ActiveRecord::Migration[6.2]
+class CreateLikes < ActiveRecord::Migration[6.1]
   def change
     create_table :likes do |t|
       t.references :user, null: false, foreign_key: true
